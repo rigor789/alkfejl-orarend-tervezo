6ea7dac614f7a067808d5161f5d05fa648694706
Igor Randjelovic <rigor789@gmail.com>
2017-11-12 15:29:23 +0100

    Added model directory, BaseEntity and User model

---
+++ src/main/java/hu/elte/alkfejl/orarend/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package hu.elte.alkfejl.orarend.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @Version
+    private int version;
+}
---
+++ src/main/java/hu/elte/alkfejl/orarend/model/User.java
@@ -0,0 +1,34 @@
+package hu.elte.alkfejl.orarend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="USERS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        USER, ADMIN, DEVELOPER
+    }
+}
