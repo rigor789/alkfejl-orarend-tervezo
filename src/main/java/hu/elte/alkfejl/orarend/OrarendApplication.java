cdc399653403e45ab7e548f6a69b061c5c6912f3
Gaál Péter <gaalpeti12@gmail.com>
2017-11-30 10:25:50 +0100

    conflict

--- src/main/java/hu/elte/alkfejl/orarend/OrarendApplication.java
+++ src/main/java/hu/elte/alkfejl/orarend/OrarendApplication.java
@@ -10,6 +10,7 @@ public class OrarendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrarendApplication.class, args);
 	}
+	//sdkajfhsdalkjfhslkfjshdfkljh
 
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**");
