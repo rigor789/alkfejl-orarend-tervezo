79af460ce6a12f4c29904431d9cd4f01500b434e
Gaál Péter <gaalpeti12@gmail.com>
2017-11-30 10:30:29 +0100

    conflict

--- src/main/java/hu/elte/alkfejl/orarend/OrarendApplication.java
+++ src/main/java/hu/elte/alkfejl/orarend/OrarendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-// touched
+// cant touch this
 @SpringBootApplication
 public class OrarendApplication {
 
